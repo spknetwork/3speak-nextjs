a007b7b76c90a0b37745e07d85705a75a1436829
chrisgraneyward <christopher.graney-ward@magic.works>
2022-11-30 12:03:21 +0000

    Replies indent instead of nest

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -6,28 +6,35 @@ import HiveMarkdown from "./HiveMarkdown";
 import Link from "next/link";
 
 export default function Comment(reply: any) {
-  return (<Paper style={{ padding: "15px 15px", margin: 10, width: '100%' }}>
-    <Grid container wrap="nowrap" spacing={2}>
-      <Grid item>
-        <UserPicture owner={reply.author} />
-      </Grid>
-      <Grid justifyContent="left" item xs zeroMinWidth>
-        <Link href={`/@${reply.author}`}>
-          <a>
-            <h4 style={{ margin: 0, textAlign: "left" }}>{reply.author}</h4>
-          </a>
-        </Link>
-        <HiveMarkdown>{reply.body}</HiveMarkdown>
-        <p style={{ textAlign: "left", color: "gray" }}>
-          {timeSince(new Date(reply.created))}
-        </p>
-      </Grid>
-    </Grid>
-    <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-    <VoteCommentPayout {...reply} style={{marginBottom: '100px'}} owner={reply.author} />
-    {reply.replies.length ? (<Divider variant="fullWidth" style={{ margin: "30px 0" }} />) : null}
-    {reply.replies.map((subReply: any) => (
-      <Comment key={`@${reply.author}/${reply.permlink}`} style={{marginTop: '1px'}} {...subReply} />
-    ))}
-  </Paper>);
+  let level = reply.level || 0
+  const maxLevel = level > 2
+  level = maxLevel ? 2 : level
+  return (
+    <>
+      <Paper style={{ padding: "15px 15px", margin: 10, width: '100%', marginLeft: `${50 * level}px` }}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <UserPicture owner={reply.author} />
+          </Grid>
+          <Grid justifyContent="left" item xs zeroMinWidth>
+            <Link href={`/@${reply.author}`}>
+              <a>
+                <h4 style={{ margin: 0, textAlign: "left" }}>{reply.author}</h4>
+              </a>
+            </Link>
+            {maxLevel && (<span style={{color: 'rgb(192,192,192)'}}>Replying to {reply.root_author}</span>)}
+            <HiveMarkdown>{reply.body}</HiveMarkdown>
+            <p style={{ textAlign: "left", color: "gray" }}>
+              {timeSince(new Date(reply.created))}
+            </p>
+          </Grid>
+        </Grid>
+        <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
+        <VoteCommentPayout {...reply} style={{marginBottom: '100px'}} owner={reply.author} />
+      </Paper>
+      {reply.replies.map((subReply: any) => (
+        <Comment key={`@${reply.author}/${reply.permlink}`} style={{marginTop: '1px'}} {...subReply} level={level + 1} />
+      ))}
+    </>
+  );
 }
