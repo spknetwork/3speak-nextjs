314f69bf88d7a8600f0134af45b769fef0999beb
igormuba <igorm-uba@hotmail.com>
2023-01-17 18:52:49 +0000

    add styles to react bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,32 @@
-import dbConnect from '../utils/dbConnect';
-import homeFeedGenerator from '../utils/getHomepage';
+import dbConnect from "../utils/dbConnect";
+import homeFeedGenerator from "../utils/getHomepage";
 import { IVideo } from "../models/Video";
-import React from 'react';
-import { VideoCard } from '../components/VideoCard';
-import { Grid, Row } from '../components/Grid';
+import React from "react";
+import { VideoCard } from "../components/VideoCard";
+import { Grid, Row } from "../components/Grid";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export async function getServerSideProps() {
   await dbConnect();
-  let homepageVideos: IVideo[] = await homeFeedGenerator()//languages);
+  let homepageVideos: IVideo[] = await homeFeedGenerator(); //languages);
   return {
-    props: { homepageVideos }
-  }
+    props: { homepageVideos },
+  };
 }
 
-export default function Home({ homepageVideos }: { homepageVideos: (IVideo & { payout: number; })[] }) {
+export default function Home({
+  homepageVideos,
+}: {
+  homepageVideos: (IVideo & { payout: number })[];
+}) {
   return (
     <Grid>
       <h1>New</h1>
       <Row>
-        {homepageVideos.map((video: IVideo & { payout: number; }) => (
+        {homepageVideos.map((video: IVideo & { payout: number }) => (
           <VideoCard key={`${video.owner}/${video.permlink}`} {...video} />
         ))}
       </Row>
     </Grid>
-  )
+  );
 }
