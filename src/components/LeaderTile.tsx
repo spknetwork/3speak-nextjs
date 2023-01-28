d4a7473d19b6df32c358187098dcdf332d78d193
igormuba <igorm-uba@hotmail.com>
2023-01-28 12:08:19 +0000

    leaderboard demo under bootstrap

---
+++ src/components/LeaderTile.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useMemo, useState } from "react";
+import RefLink from "./RefLink";
+
+export interface LeaderTileProps {
+  info: {
+    score: number;
+    rank: number;
+  };
+  reflink: string;
+}
+
+export function LeaderTile(props: LeaderTileProps) {
+  const reflink = useMemo(() => {
+    return RefLink.parse(props.reflink);
+  }, [props.reflink]);
+
+  const [profilePicture, setProfilePicture] = useState("");
+  const [borderLeftCode, setBorderLeftCode] = useState("");
+
+  useEffect(() => {
+    const load = async () => {
+      console.log(`displaying leader tile`, props);
+      let color: string;
+      switch (props.info.rank) {
+        case 1: {
+          color = "#d4af37 solid 6px";
+          break;
+        }
+        case 2: {
+          color = "#bec2cb solid 6px";
+          break;
+        }
+        case 3: {
+          color = "#b08d57 solid 6px";
+          break;
+        }
+      }
+      console.log("props.reflink");
+      console.log(props.reflink.split("hive:"));
+
+      setProfilePicture(
+        `https://images.hive.blog/u/${props.reflink.split("hive:")[1]}/avatar`
+      );
+      setBorderLeftCode(color);
+    };
+
+    void load();
+  }, []);
+
+  return (
+    <div className="channels-card" style={{ borderLeft: borderLeftCode }}>
+      <div className="channels-card-image">
+        <a href={`#/user/${props.reflink.toString()}`}>
+          <img className="img-fluid" src={profilePicture} alt="" />
+        </a>
+        <div className="channels-card-image-btn">
+          <a
+            href={`#/user/${reflink.toString()}`}
+            className="btn btn-outline-primary btn-sm"
+          >
+            View Channel
+          </a>
+        </div>
+      </div>
+      <div className="channels-card-body">
+        <div className="channels-title">
+          <a href={`#/user/${reflink.toString()}`}>{reflink.root}</a>
+        </div>
+        <div>
+          <i></i>
+        </div>
+      </div>
+      <div className="channels-card-rank badge badge-dark text-white">
+        Rank: {props.info.rank}
+      </div>
+      <div className="channels-card-score badge badge-dark text-white">
+        Score: {Math.round(props.info.score)}
+      </div>
+    </div>
+  );
+}
--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { LeaderTile } from "@/components/LeaderTile";
 
-const Leaderboard = () => {
-  return <div>Leaderboard</div>;
-};
+export default function LeaderboardView() {
+  const [first, setFirst] = useState<any>();
+  const [second, setSecond] = useState<any>();
+  const [third, setThird] = useState<any>();
+  const [bronze, setBronze] = useState<any[]>([]);
 
-export default Leaderboard;
+  useEffect(() => {
+    let data = [
+      { rank: 1, score: 519, username: "rairfoundation" },
+      { rank: 2, score: 505, username: "rair2" },
+      { rank: 3, score: 400, username: "pouch22" },
+      { rank: 4, score: 399, username: "igormuba" },
+      { rank: 5, score: 398, username: "igormuba-dev" },
+    ];
+    console.log(data);
+    let step = 1;
+    let bronzeToSet = [];
+    for (const ex of data) {
+      if (step >= 30) {
+        break;
+      }
+      if (step === 1) {
+        console.log("FIRST");
+        console.log(ex);
+
+        setFirst(ex);
+      } else if (step === 2) {
+        console.log("SECOND");
+        console.log(ex);
+        setSecond(ex);
+      } else if (step === 3) {
+        console.log("THIRD");
+        console.log(ex);
+        setThird(ex);
+      } else {
+        bronzeToSet.push(ex);
+      }
+      step++;
+    }
+    setBronze(bronzeToSet);
+
+    document.title = "3Speak - Tokenised video communities";
+    void load();
+
+    async function load() {
+      let step = 1;
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(`first is now `, first);
+  }, [first]);
+
+  return (
+    <div>
+      <div className="header_sec">
+        <Container fluid className="header_sec">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-xs-12 header_dist1">
+              <h1 className="white_col">Content Creator Leaderboard</h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+      <section className="content_home">
+        <Container fluid>
+          <Row className="justify-content-md-center">
+            <div className="col-xl-8 col-sm-8 col-12 mb-3">
+              {first && (
+                <LeaderTile info={first} reflink={`hive:${first.username}`} />
+              )}
+            </div>
+          </Row>
+          <Row className="justify-content-md-center">
+            <div className="col-xl-5 col-sm-8 col-12 mb-3">
+              {second ? (
+                <LeaderTile info={second} reflink={`hive:${second.username}`} />
+              ) : null}
+            </div>
+            <div className="col-xl-5 col-sm-8 col-12 mb-3">
+              {third ? (
+                <LeaderTile info={third} reflink={`hive:${third.username}`} />
+              ) : null}
+            </div>
+            <Row>
+              {bronze.map((value) => (
+                <div key={value.username} className="col-xl-2 col-sm-4 mb-3">
+                  <LeaderTile info={value} reflink={`hive:${value.username}`} />
+                </div>
+              ))}
+            </Row>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
+}
