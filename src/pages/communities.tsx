e23f9e7d2ac21fe7a959ea537f3402d25499d2e4
Juneroy Derano <eroyjune@gmail.com>
2023-02-21 19:38:31 +0800

    feat: enhanced the UI for communities page ---- DONE

--- src/pages/communities.tsx
+++ src/pages/communities.tsx
@@ -19,27 +19,58 @@ export default function CommunitiesView() {
     generate();
   }, []);
   return (
-    <Container fluid>
-      <Row>
-        <div className="col-12">
-          <h3 style={{ display: "inline-block" }}>Communities</h3>
-          <span className="float-right mb-3">
-            <Button id="communityCreate" variant="primary" disabled>
-              Create +
-            </Button>
-          </span>
-        </div>
-      </Row>
-      <Row>
-        {data.map((value: any) => (
-          <CommunityTile
-            key={value.name}
-            name={value.name}
-            reflink={`hive:${value.name}`}
-            info={value}
-          />
-        ))}
-      </Row>
-    </Container>
+    <div>
+      <div className="header_sec">
+        <Container fluid className="header_sec">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-xs-12 header_dist1">
+              <h1 className="white_col">COMMUNITIES</h1>
+              <p className="text-dark">
+                Keep scrolling to explore more exciting communities!
+              </p>
+            </div>
+          </div>
+        </Container>
+      </div>
+      <Container fluid className="p-3 bg-color-eff4f5">
+        {/* <Row>
+          <div className="col-md-12">
+
+          </div>
+        </Row> */}
+        <Row>
+          <div className="col-12 d-flex flex-row justify-content-between">
+            <h3 style={{ display: "inline-block" }}>Communities</h3>
+            <span className="float-right mb-3">
+              <Button
+                className="color-primary p-3"
+                id="communityCreate"
+                variant="primary"
+              >
+                CREATE +
+              </Button>
+            </span>
+          </div>
+
+          <div className="col-12">
+            <div className="alert alert-primary text-white" role="alert">
+              Make sure you have a minimum of 3.00 HIVE in your HIVE wallet to
+              create a new community
+            </div>
+          </div>
+        </Row>
+        <Row>
+          {data.map((value: any) => (
+            <CommunityTile
+              key={value.name}
+              name={value.name}
+              sum_pending={value.sum_pending}
+              reflink={`hive:${value.name}`}
+              info={value}
+            />
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 }
