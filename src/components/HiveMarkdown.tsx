fbb2919a22b0a59416c0b0b53456569384e8edf7
igormuba <igorm-uba@hotmail.com>
2023-03-08 17:58:48 +0000

    limit image size to 100%

--- src/components/HiveMarkdown.tsx
+++ src/components/HiveMarkdown.tsx
@@ -1,24 +1,43 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function HiveMarkdown(props: any) {
   const [expanded, setExpanded] = useState(false);
-  
+
   const body = props.children.replace(
     /(@\b[a-zA-Z]*)\b(?![^[]*\])(?![^>]*\<\/ ?a)/g,
-    (username: string) => (`[${username}](/${username})`)
-  )
-  const maxLength = 300
+    (username: string) => `[${username}](/${username})`
+  );
+  const maxLength = 300;
+  useEffect(() => {
+    console.log("body");
+    console.log(body);
+  }, [body]);
+  useEffect(() => {
+    console.log("props.children");
+    console.log(props.children);
+  }, [props.children]);
 
   return (
     <span>
-      <ReactMarkdown remarkPlugins={[[remarkGfm]]} rehypePlugins={[rehypeRaw]}>
+      <ReactMarkdown
+        components={{
+          img: ({ node, ...props }) => (
+            <img style={{ maxWidth: "100%" }} {...props} />
+          ),
+        }}
+        remarkPlugins={[[remarkGfm]]}
+        rehypePlugins={[rehypeRaw]}
+      >
         {expanded ? body : body.slice(0, maxLength)}
       </ReactMarkdown>
       {body.length > maxLength && (
-        <div onClick={() => setExpanded(!expanded)} style={{color: 'rgb(192,192,192)', cursor: 'pointer'}}>
+        <div
+          onClick={() => setExpanded(!expanded)}
+          style={{ color: "rgb(192,192,192)", cursor: "pointer" }}
+        >
           Show {expanded ? "less" : "more"}
         </div>
       )}
--- src/pages/[author]/[permlink].tsx
+++ src/pages/[author]/[permlink].tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import JWPlayer from 'react-jw-player';
+import JWPlayer from "react-jw-player";
 import dbConnect from "@/utils/dbConnect";
 import { GetServerSidePropsContext } from "next";
-import { Col, Grid, Row } from "src/components/Grid";
+// import { Col, Grid, Row } from "src/components/Grid";
 import Link from "next/link";
 import { SuggestedVideoCard } from "src/components/SuggestedVideoCard";
 import { VoteCommentPayout } from "src/components/VoteCommentPayout";
@@ -10,32 +10,33 @@ import { Subscribe } from "src/components/Subscribe";
 import { getFollowerInfo, getReplies } from "@/utils/hive";
 import getVideo from "@/utils/getVideo";
 import getSimilarVideos from "@/utils/getSimilarVideos";
-import getOtherVideosByOwner from '@/utils/getOtherVideosByOwner';
+import getOtherVideosByOwner from "@/utils/getOtherVideosByOwner";
 import { IVideo } from "src/models/Video";
 import CommentSection from "src/components/CommentSection";
 import { Button } from "@mui/material";
 import HiveMarkdown from "src/components/HiveMarkdown";
-
-export async function getServerSideProps(context: GetServerSidePropsContext<{ author: string; permlink: string; }>) {
+import { Container, Row, Col } from "react-bootstrap";
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ author: string; permlink: string }>
+) {
   await dbConnect();
 
-  let { author, permlink } = context.params || {author: '', permlink: ''}
-  author = author.replace('@', '')
-  const authorperm = {owner: author, permlink}
+  let { author, permlink } = context.params || { author: "", permlink: "" };
+  author = author.replace("@", "");
+  const authorperm = { owner: author, permlink };
 
   const video = await getVideo(authorperm);
-  const otherVideosByOwner = await getOtherVideosByOwner(authorperm)
+  const otherVideosByOwner = await getOtherVideosByOwner(authorperm);
   // const similarVideos = await getSimilarVideos(video, author)
   // const followerInfo = await getFollowerInfo(video);
   // const replies = await getReplies({author, permlink});
-  const similarVideos: any[] = []
-  const followerInfo: any[] = []
-  const replies: any[] = []
-
+  const similarVideos: any[] = [];
+  const followerInfo: any[] = [];
+  const replies: any[] = [];
 
   return {
-    props: { video, otherVideosByOwner, similarVideos, followerInfo, replies }
-  }
+    props: { video, otherVideosByOwner, similarVideos, followerInfo, replies },
+  };
 }
 
 const Watch = ({
@@ -43,7 +44,7 @@ const Watch = ({
   otherVideosByOwner,
   similarVideos,
   followerInfo,
-  replies
+  replies,
 }: {
   video: IVideo & {
     playUrl: string;
@@ -51,68 +52,93 @@ const Watch = ({
     upvotes: any[];
     downvotes: any[];
   };
-  otherVideosByOwner: (IVideo & { payout: number; })[];
-  similarVideos: (IVideo & { payout: number; })[];
+  otherVideosByOwner: (IVideo & { payout: number })[];
+  similarVideos: (IVideo & { payout: number })[];
   followerInfo: {
     count: number;
     following: boolean;
   };
-  replies: any[]
+  replies: any[];
 }) => {
   return (
-    <Grid>
+    <Container>
       <Row>
-        <Col size={2}>
+        <Col sm xs={2}>
           <JWPlayer
-            playerId='video-player'
+            playerId="video-player"
             file={video.playUrl}
             image={video.thumbUrl}
-            playerScript='https://cdn.jwplayer.com/libraries/j7Kz0Rae.js'
+            playerScript="https://cdn.jwplayer.com/libraries/j7Kz0Rae.js"
           />
           <Row>
-            <Col size={3} style={{paddingLeft: 10}}>
+            <Col sm xs={10} style={{ paddingLeft: 10 }}>
               <h3>{video.title}</h3>
               <Subscribe owner={video.owner} followerInfo={followerInfo} />
-              <>{video.views} views{video.community ? (
-              <> &#x2022; <Link href={`/c/${video.hive}`}><a>{video.community}</a></Link></>) : <></>}</>
+              <>
+                {video.views} views
+                {video.community ? (
+                  <>
+                    {" "}
+                    &#x2022;{" "}
+                    <Link href={`/c/${video.hive}`}>
+                      <a>{video.community}</a>
+                    </Link>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </>
             </Col>
-            <Col size={1} style={{textAlign: 'right', paddingTop: 20}}>
+            <Col sm xs={2} style={{ textAlign: "right", paddingTop: 20 }}>
               <VoteCommentPayout {...video} />
-              <br /><br />
-              <Button href={`/openDapp?uri=hive:${video.owner}:${video.permlink}`}>Open in the desktop app</Button>
+              <br />
+              <br />
+              <Button
+                href={`/openDapp?uri=hive:${video.owner}:${video.permlink}`}
+              >
+                Open in the desktop app
+              </Button>
             </Col>
           </Row>
           <Row>
-            <HiveMarkdown>
-              {video.description}
-            </HiveMarkdown>
+            <Col sm xs={10}>
+              <HiveMarkdown>{video.description}</HiveMarkdown>
+            </Col>
+            <Col sm xs={2}></Col>
           </Row>
           <Row>
             <h1>Comments</h1>
             <CommentSection replies={replies} />
           </Row>
         </Col>
-        <Col size={1} style={{paddingLeft: '10px'}}>
-          {
-            otherVideosByOwner.length ?
-              <>
-                <Link href={`/@${video.owner}`}><a><h4>More from {video.owner}</h4></a></Link>
-                {otherVideosByOwner.map(videoFromOwner => (
-                  <SuggestedVideoCard key={`${videoFromOwner.owner}/${videoFromOwner.permlink}`} {...videoFromOwner} />
-                ))}
-              </> : null
-          }
+        <Col sm xs={2} style={{ paddingLeft: "10px" }}>
+          {otherVideosByOwner.length ? (
+            <>
+              <Link href={`/@${video.owner}`}>
+                <a>
+                  <h4>More from {video.owner}</h4>
+                </a>
+              </Link>
+              {otherVideosByOwner.map((videoFromOwner) => (
+                <SuggestedVideoCard
+                  key={`${videoFromOwner.owner}/${videoFromOwner.permlink}`}
+                  {...videoFromOwner}
+                />
+              ))}
+            </>
+          ) : null}
           <h4>More Videos</h4>
           <>
-            {
-              similarVideos.map(videoFromOther => (
-                <SuggestedVideoCard key={`${videoFromOther.owner}/${videoFromOther.permlink}`} {...videoFromOther} />
-              ))
-            }
+            {similarVideos.map((videoFromOther) => (
+              <SuggestedVideoCard
+                key={`${videoFromOther.owner}/${videoFromOther.permlink}`}
+                {...videoFromOther}
+              />
+            ))}
           </>
         </Col>
       </Row>
-    </Grid>
+    </Container>
   );
 };
 
