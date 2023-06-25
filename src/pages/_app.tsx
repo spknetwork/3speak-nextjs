1a028b9d2a594ede247cc85cea3bca07e4f2b080
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-25 21:00:18 +0800

    feat: added enhancement of css in footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, Image, Link, Text } from "@chakra-ui/react";
 
 import { ApolloProvider } from "@apollo/client";
 import client from "../lib/apolloClient";
@@ -49,7 +49,6 @@ function MyApp({ Component, pageProps }: AppProps) {
               flex-direction: column;
             }
           `}
-          
         >
           <main>
             <Box width={"100%"} backgroundColor="#EFF4F6">
@@ -70,7 +69,67 @@ function MyApp({ Component, pageProps }: AppProps) {
             </Box>
           </main>
 
-          <Box marginTop={'auto'}>Footer body</Box>
+          <Box backgroundColor={'#e8e8e8'} padding='5px' paddingTop={'10px'} marginTop={"auto"}>
+            <Box
+              paddingX={'15px'}
+              marginX={"auto"}
+              maxWidth="1140px"
+              width={"100%"}
+              margin="auto"
+            >
+              <Flex width={"100%"}>
+                <Box flex={"0 0 25%"} maxWidth={"25%"} paddingX="20px">
+                  <Image
+                    src="/images/3S_logo.svg"
+                    alt="3speak logo"
+                    width={200}
+                    height={100}
+                  />
+                  <Link href="#">
+                    <Text fontWeight={"bold"} margin={"0px"}>
+                      helpdesk@3speak.tv
+                    </Text>
+                  </Link>
+                  <Link href="#">
+                    <Text fontWeight={"bold"} margin={"0px"}>
+                      3speak.tv
+                    </Text>
+                  </Link>
+                </Box>
+                <Box flex={"0 0 25%"} maxWidth={"25%"} paddingX="20px">
+                  <Text fontWeight={"bold"} margin={"0px"} marginBottom="20px">
+                    company
+                  </Text>
+                  <Link href="#">
+                    <Text fontWeight={"500"} margin={"0px"}>
+                      About us
+                    </Text>
+                  </Link>
+                  <Link href="#">
+                    <Text fontWeight={"500"} margin={"0px"}>
+                      FAQ
+                    </Text>
+                  </Link>
+                  <Link href="#">
+                    <Text fontWeight={"500"} margin={"0px"}>
+                      Terms
+                    </Text>
+                  </Link>
+                </Box>
+                <Box flex={"0 0 25%"} maxWidth={"25%"} paddingX="20px">
+                  <Text fontWeight={"bold"} margin={"0px"} marginBottom="20px">
+                    Accepted Payment Methods
+                  </Text>
+                  <Image
+                    src="/images/hive-blockchain-hive-logo.svg"
+                    alt="3speak logo"
+                    width={200}
+                    height={100}
+                  />
+                </Box>
+              </Flex>
+            </Box>
+          </Box>
         </Flex>
       </Flex>
 
