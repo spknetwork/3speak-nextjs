1bef3ddbb7c88715ab5f01779e5a169cdcc803f6
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-26 19:58:49 +0800

    feat: refactor code for upload page

--- src/pages/auth/upload.tsx
+++ src/pages/auth/upload.tsx
@@ -1,82 +1,20 @@
 import React, { ReactNode } from "react";
-import styled from "styled-components";
 
 import {
-  IconButton,
-  Avatar,
   Box,
-  CloseButton,
   Flex,
-  HStack,
-  VStack,
-  Icon,
   useColorModeValue,
-  Link,
   Drawer,
   DrawerContent,
   Text,
   useDisclosure,
-  BoxProps,
-  FlexProps,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Image,
-  Heading,
   Card,
   CardBody,
-  SimpleGrid,
-  CardHeader,
-  Stack,
-  StackDivider,
 } from "@chakra-ui/react";
-import {
-  FiHome,
-  FiUpload,
-  FiVideo,
-  FiUsers,
-  FiSettings,
-  FiMenu,
-  FiBell,
-  FiChevronDown,
-} from "react-icons/fi";
-import { IconType } from "react-icons";
-import { ReactText } from "react";
-import {
-  FaArchive,
-  FaChartLine,
-  FaCloudUploadAlt,
-  FaCog,
-  FaExternalLinkAlt,
-  FaRegEye,
-  FaSignOutAlt,
-  FaUserAlt,
-  FaUsers,
-  FaVideo,
-  FaWallet,
-} from "react-icons/fa";
 import { SlPicture } from "react-icons/sl";
 import { useRouter } from "next/router";
-
-interface LinkItemProps {
-  name: string;
-  icon: IconType;
-  route?: string;
-}
-const LinkItems: Array<LinkItemProps> = [
-  {
-    name: "Dashboard",
-    icon: FiHome,
-    route: "/auth/studio",
-  },
-  { name: "Upload", icon: FaCloudUploadAlt, route: "/auth/upload" },
-  { name: "Videos", icon: FaVideo, route: "/auth/studio_videos" },
-
-  { name: "My Channel", icon: FaExternalLinkAlt, route: "/auth/studio_videos" },
-  { name: "Logout", icon: FaSignOutAlt, route: "/auth/studio_videos" },
-];
+import SidebarContent from "@/components/studio_sidebar/StudioSidebar";
+import MobileNav from "@/components/studio_mobilenav/StudioMobileNav";
 
 export default function SidebarWithHeader({
   children,
@@ -199,7 +137,7 @@ export default function SidebarWithHeader({
                         </Text>
                       </Box>
                       <Flex
-                        cursor={'pointer'}
+                        cursor={"pointer"}
                         onClick={() => router.push("/auth/create_post")}
                         width={{ base: "300px", md: "300px", lg: "372px" }}
                         height={{ base: "200px", md: "200px", lg: "332px" }}
@@ -253,191 +191,3 @@ export default function SidebarWithHeader({
     </Box>
   );
 }
-
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
-
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
- 
-
-  return (
-    <Box
-      transition="3s ease"
-      bg={useColorModeValue("white", "gray.900")}
-      borderRight="1px"
-      borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}
-    >
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        {/* <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text> */}
-        <Flex justifyContent="center" alignItems={"center"} width="100%">
-          <StyledLink href="/auth/studio">
-            <Box
-              display={"flex"}
-              justifyContent="center"
-              alignItems={"center"}
-              width={"180px"}
-            >
-              <Image
-                src="/main_logo.svg"
-                alt="3speak logo"
-                width={100}
-                height={100}
-              />
-            </Box>
-          </StyledLink>
-        </Flex>
-        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
-      </Flex>
-      {LinkItems.map((link) => (
-        <NavItem
-          route={link.route}
-          color={"#6e707e"} key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
-    </Box>
-  );
-};
-
-interface NavItemProps extends FlexProps {
-  icon: IconType;
-  route: string | any;
-  children: ReactText;
-}
-const NavItem = ({ icon, route, children, ...rest }: NavItemProps) => {
-  const router = useRouter();
-  return (
-    // // <Link
-   
-    // //   href="#"
-    // //   style={{ textDecoration: "none" }}
-    // //   _focus={{ boxShadow: "none" }}
-    // // >
-    //   {/* _hover=
-    //   {{
-    //     bg: "gray.400",
-    //     color: "white",
-    //   }} */}
-      <Flex
-        onClick={() => router.push(route) }
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        {...rest}
-      >
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "black",
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
-    // </Link>
-  );
-};
-
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
-}
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}
-    >
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text
-        display={{ base: "flex", md: "none" }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold"
-      >
-        Logo
-      </Text>
-
-      <HStack spacing={{ base: "0", md: "6" }}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiBell />}
-        />
-        <Flex alignItems={"center"}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: "none" }}
-            >
-              <HStack>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("white", "gray.900")}
-              borderColor={useColorModeValue("gray.200", "gray.700")}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
-      </HStack>
-    </Flex>
-  );
-};
-
-const StyledLink = styled(Link)`
-  cursor: pointer;
-`;
