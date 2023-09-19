f12b8f15e2c7f2877f524ca2a2cea004f1c7aa0b
Juneroy Quinimon <eroyjune@gmail.com>
2023-09-20 01:08:14 +0800

    feat: update store for the new added state module for user hive details

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,10 +3,12 @@
 import create from 'zustand'
 import { createStudioSlice, StudioSlice } from './slices/createStudioSlice'
 import { createAuthUserSlice, AuthUserSlice } from './slices/createAuthStore'
+import { createUserDetailsSlice, UserSlice } from './slices/createUserStore'
 
-type StoreState = StudioSlice & AuthUserSlice
+type StoreState = StudioSlice & AuthUserSlice & UserSlice
 
 export const useAppStore = create<StoreState>()((...a) => ({
     ...createStudioSlice(...a),
     ...createAuthUserSlice(...a),
+    ...createUserDetailsSlice(...a),
 }))
