4137c4565488b5f20126c40d173e7e69fd8eb3c7
Juneroy Quinimon <eroyjune@gmail.com>
2023-11-02 23:56:33 +0800

    feat: fixed UI in right side components in watch page

--- src/components/watch/Video.tsx
+++ src/components/watch/Video.tsx
@@ -1,10 +1,12 @@
-import { Box, Image, Link } from "@chakra-ui/react";
+import { Box, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
+import { MdPlayArrow } from "react-icons/md";
 
 const Video = ({ videoSrc }: any) => {
   return (
     <Box w="100%" bg="white.500" color={"black"} marginBottom="3rem">
       <Box
+        cursor={'pointer'}
         float="left"
         w="calc(136px * 1.25)"
         height="calc(77px * 1.25)"
@@ -14,11 +16,29 @@ const Video = ({ videoSrc }: any) => {
         position={"relative"}
       >
         <Box
+          display={'flex'}
+          justifyContent='center'
+          alignItems={'center'}
           position={"absolute"}
           bottom="5px"
           color={"#000"}
           fontSize="11px"
           fontWeight={"500"}
+          left="5px"
+          background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
+          borderRadius="2px"
+          paddingLeft={"4px"}
+          paddingRight={"8px"}
+        >
+          <MdPlayArrow size='18px' color="grey" />
+          <Text as='span' marginLeft={'2px'} fontSize='11px' fontWeight={'bold'}>12</Text>
+        </Box>
+        <Box
+          position={"absolute"}
+          bottom="5px"
+          color={"#000"}
+          fontSize="11px"
+          fontWeight={"bold"}
           right="5px"
           background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
           borderRadius="2px"
