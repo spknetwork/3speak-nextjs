f66da2f7f6d416473343c43cf8899ff50290ad72
Juneroy Quinimon <eroyjune@gmail.com>
2024-02-27 14:02:02 +0800

    feat: added tags

--- src/components/watch/Title.tsx
+++ src/components/watch/Title.tsx
@@ -1,7 +1,43 @@
-import { Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
-const Title = () => {
+const Title = ({getVideo}:any) => {
+  const [videoUrl, setvideoUrl] = useState<any>(null)
+  const [videoUrlSelected, setvideoUrlSelected] = useState<any>(null)
+  useEffect(() => {
+    if (videoUrl) {
+      // console.log("setvideoUrl4 final step",videoUrl)
+    }
+   
+  },[videoUrl])
+
+  useEffect(() => {
+    if (getVideo) {
+    // console.log("getVideo in player 3", getVideo.spkvideo.play_url)
+    if (getVideo.spkvideo.play_url) {
+      const url = getVideo.spkvideo.play_url
+      // Splitting the string by "ipfs://" and getting the first result
+      const splitResult = url.split("ipfs://");
+
+      // The first element after splitting might be an empty string if the string starts with "ipfs://"
+      // So, we check if the first element is empty and select the second element in that case
+      const result = splitResult[0] === "" ? splitResult[1] : splitResult[0];
+      setvideoUrlSelected("https://ipfs-3speak.b-cdn.net/ipfs/"+result)
+    }
+    // console.log("ipfs://QmPX8YosD35YphprEi5apHzbCcXXzq1xZbDdFiv7qJVFXv/manifest.m3u8")
+    setvideoUrl(getVideo.spkvideo)
+    }
+  },[getVideo])
+
+  useEffect(() => {
+    console.log("videoUrlSelected", videoUrlSelected)
+  },[videoUrlSelected])
+  // "https://ipfs-3speak.b-cdn.net/ipfs/QmWoqdoLtsF4obB5sfSUc3GEZGY87TmcJrt6JpH8bJqsuK/manifest.m3u8" thumbnail_url
+  // "https://ipfs-3speak.b-cdn.net/ipfs/bafybeibqxbf652lmfbdf7zoht3pbhkx4m76agdwn5mnw33vjhlxrzvccoe/"
+  // `${videoUrl.play_url}` ipfs://QmPX8YosD35YphprEi5apHzbCcXXzq1xZbDdFiv7qJVFXv/manifest.m3u8
+  if (!videoUrl) {
+    return <Box>getting video details</Box>;
+  }
   return (
     <Text
       fontSize={"20px"}
@@ -11,7 +47,7 @@ const Title = () => {
       marginTop={"0.5rem"}
       lineHeight={"38px"}
     >
-      (Esp/Eng) ¡Mis Tres hábitos para un día feliz!
+      {getVideo.title}
     </Text>
   );
 };
