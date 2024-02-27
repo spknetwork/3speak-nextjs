99dd4000f7e70ea222c5c414e15bc50c3daa0690
Juneroy Quinimon <eroyjune@gmail.com>
2024-02-27 14:04:09 +0800

    feat: get video details in profile component

--- src/components/watch/Profile.tsx
+++ src/components/watch/Profile.tsx
@@ -1,8 +1,44 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Avatar, Box, Button, Flex, Link, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Profile = () => {
+const Profile = ({ getVideo }: any) => {
+  const [videoUrl, setvideoUrl] = useState<any>(null)
+  const [videoUrlSelected, setvideoUrlSelected] = useState<any>(null)
+  useEffect(() => {
+    if (videoUrl) {
+      // console.log("setvideoUrl4 final step",videoUrl)
+    }
+
+  }, [videoUrl])
+
+  useEffect(() => {
+    if (getVideo) {
+      // console.log("getVideo in player 3", getVideo.spkvideo.play_url)
+      if (getVideo.spkvideo.play_url) {
+        const url = getVideo.spkvideo.play_url
+        // Splitting the string by "ipfs://" and getting the first result
+        const splitResult = url.split("ipfs://");
+
+        // The first element after splitting might be an empty string if the string starts with "ipfs://"
+        // So, we check if the first element is empty and select the second element in that case
+        const result = splitResult[0] === "" ? splitResult[1] : splitResult[0];
+        setvideoUrlSelected("https://ipfs-3speak.b-cdn.net/ipfs/" + result)
+      }
+      // console.log("ipfs://QmPX8YosD35YphprEi5apHzbCcXXzq1xZbDdFiv7qJVFXv/manifest.m3u8")
+      setvideoUrl(getVideo.spkvideo)
+    }
+  }, [getVideo])
+
+  useEffect(() => {
+    console.log("videoUrlSelected", videoUrlSelected)
+  }, [videoUrlSelected])
+  // "https://ipfs-3speak.b-cdn.net/ipfs/QmWoqdoLtsF4obB5sfSUc3GEZGY87TmcJrt6JpH8bJqsuK/manifest.m3u8" thumbnail_url
+  // "https://ipfs-3speak.b-cdn.net/ipfs/bafybeibqxbf652lmfbdf7zoht3pbhkx4m76agdwn5mnw33vjhlxrzvccoe/"
+  // `${videoUrl.play_url}` ipfs://QmPX8YosD35YphprEi5apHzbCcXXzq1xZbDdFiv7qJVFXv/manifest.m3u8
+  if (!videoUrl) {
+    return <Box>getting video details</Box>;
+  }
   return (
     <Flex justifyContent={"space-between"}>
       <Box bg="white" p={4} color="black">
@@ -13,7 +49,7 @@ const Profile = () => {
             src="https://bit.ly/dan-abramov"
           />
           <Flex flexDirection={"column"} className="ms-4">
-            <Link fontWeight={"bolder"}>stellamartinez</Link>
+            <Link fontWeight={"bolder"}>{getVideo.author.username}</Link>
             {/* <Flex justifyContent={"start"}>
               <Box bg="white" p={4} paddingLeft="0px" color="black">
                 <Text marginBottom={"10px"} fontSize={"11px"}>
