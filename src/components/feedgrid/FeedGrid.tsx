f93990c6e966d62f8aa1f388427a18dc99ef2fd4
Sagar <sagar.devices@gmail.com>
2024-03-19 23:07:57 +0530

    using feed grid component

---
+++ src/components/feedgrid/FeedGrid.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { VideoInterface } from "types";
+import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import { MdOutlineThumbUp } from "react-icons/md";
+import { BiDollar } from "react-icons/bi";
+
+import VideosTitle from "@/components/VideosTitle";
+import Name from "@/components/user/Name";
+
+import moment from "moment";
+
+// https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript/34841026#34841026
+var toHHMMSS = (secs: number) => {
+  var sec_num = parseInt(secs.toFixed(1), 10)
+  var hours   = Math.floor(sec_num / 3600)
+  var minutes = Math.floor(sec_num / 60) % 60
+  var seconds = sec_num % 60
+
+  return [hours,minutes,seconds]
+      .map(v => v < 10 ? "0" + v : v)
+      .filter((v,i) => v !== "00" || i > 0)
+      .join(":")
+}
+
+const FeedGrid = ({ videos }: any) => {
+  return (
+    <Grid
+      padding={"20px"}
+      templateColumns={{
+        base: "repeat(1, 1fr)",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(2, 1fr)",
+        xl: "repeat(5, 1fr)",
+      }}
+      gap={10}
+    >
+      {videos.map((video: any, index: number) => (
+        <GridItem w="100%" h="100%" key={index}>
+          <Box cursor={"pointer"} position="relative">
+            <Box
+              display={"flex"}
+              justifyContent="center"
+              alignItems={"center"}
+              position={"absolute"}
+              bottom="5px"
+              color={"#000"}
+              fontSize="11px"
+              fontWeight={"500"}
+              left="5px"
+              background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
+              borderRadius="2px"
+              paddingLeft={"4px"}
+              paddingRight={"8px"}
+            ></Box>
+            <Box
+              display={"flex"}
+              justifyContent="center"
+              alignItems={"center"}
+              position={"absolute"}
+              bottom="5px"
+              color={"#000"}
+              left="5px"
+              fontSize="11px"
+              fontWeight={"500"}
+              background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
+              borderRadius="2px"
+              paddingLeft={"4px"}
+              paddingRight={"4px"}
+            >
+              <MdOutlineThumbUp size="15px" color="grey" />
+              <Text as="span" fontSize="11px" fontWeight={"bold"}>
+                {`${video.stats.num_votes}`}
+              </Text>
+            </Box>
+
+            <Box
+              display={"flex"}
+              justifyContent="center"
+              alignItems={"center"}
+              position={"absolute"}
+              bottom="5px"
+              left="52px"
+              color={"#000"}
+              fontSize="11px"
+              fontWeight={"500"}
+              background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
+              borderRadius="2px"
+              paddingLeft={"4px"}
+              paddingRight={"4px"}
+            >
+              <BiDollar size="12px" color="black" />
+              <Text
+                as="span"
+                marginLeft={"2px"}
+                fontSize="11px"
+                fontWeight={"bold"}
+              >
+                {video.stats.total_hive_reward}
+              </Text>
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom="5px"
+              color={"#000"}
+              fontSize="11px"
+              fontWeight={"bold"}
+              right="5px"
+              background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
+              borderRadius="2px"
+              padding={"0px 6px"}
+            >
+              {toHHMMSS(video.spkvideo.duration)}
+            </Box>
+            <Box height="13em !important" width="100% !important">
+              <Image
+                height="13em !important"
+                width="100% !important"
+                borderRadius={"10px"}
+                objectFit="cover"
+                alt="test"
+                src={`https://images.hive.blog/320x0/${video.spkvideo.thumbnail_url}`}
+              />
+            </Box>
+          </Box>
+          <VideosTitle title={`${video.title}`} />
+          <Name username={`${video.author.username}`} />
+          <Text as="p" margin={"1px"}>
+            {moment(video.created_at).fromNow()}
+          </Text>
+        </GridItem>
+      ))}
+    </Grid>
+  );
+};
+
+export default FeedGrid;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
-import VideosTitle from "@/components/VideosTitle";
-import Name from "@/components/user/Name";
 import {
   Box,
   Flex,
-  Grid,
-  GridItem,
-  Image,
   Switch,
   Text,
   useColorMode,
@@ -13,16 +8,16 @@ import {
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { VideoInterface } from "types";
-import { MdPlayArrow } from "react-icons/md";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { css } from "@emotion/react";
 import { Sidebar } from "@/components";
 import MainLayout from "@/components/Layouts/main_layout";
 import { videoData } from "../data/constData";
-import { BiDollar } from "react-icons/bi";
 
 import { useQuery } from "@apollo/react-hooks";
 import { GET_TRENDING_FEED } from "../graphql/queries";
+
+import FeedGrid from "../components/feedgrid/FeedGrid";
 
 const NewIndex = () => {
   const bgColor = useColorModeValue("white", "gray.800");
@@ -53,113 +48,11 @@ const NewIndex = () => {
             {colorMode !== "dark" && <SunIcon />}
           </Text>
         </Flex>
-        {getTrendingFeed.loading ? <h1>Loading</h1> : <h1>done loading</h1>}
-        <Grid
-          padding={"20px"}
-          templateColumns={{
-            base: "repeat(1, 1fr)",
-            md: "repeat(2, 1fr)",
-            lg: "repeat(2, 1fr)",
-            xl: "repeat(5, 1fr)",
-          }}
-          gap={10}
-        >
-          {videos.map((video: VideoInterface, index: number) => (
-            <GridItem w="100%" h="100%" key={index}>
-              <Box cursor={"pointer"} position="relative">
-                <Box
-                  display={"flex"}
-                  justifyContent="center"
-                  alignItems={"center"}
-                  position={"absolute"}
-                  bottom="5px"
-                  color={"#000"}
-                  fontSize="11px"
-                  fontWeight={"500"}
-                  left="5px"
-                  background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
-                  borderRadius="2px"
-                  paddingLeft={"4px"}
-                  paddingRight={"8px"}
-                ></Box>
-                <Box
-                  display={"flex"}
-                  justifyContent="center"
-                  alignItems={"center"}
-                  position={"absolute"}
-                  bottom="5px"
-                  color={"#000"}
-                  left="5px"
-                  fontSize="11px"
-                  fontWeight={"500"}
-                  background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
-                  borderRadius="2px"
-                  paddingLeft={"4px"}
-                  paddingRight={"4px"}
-                >
-                  <MdPlayArrow size="15px" color="grey" />
-                  <Text as="span" fontSize="11px" fontWeight={"bold"}>
-                    {video.number_views}
-                  </Text>
-                </Box>
-
-                <Box
-                  display={"flex"}
-                  justifyContent="center"
-                  alignItems={"center"}
-                  position={"absolute"}
-                  bottom="5px"
-                  left="52px"
-                  color={"#000"}
-                  fontSize="11px"
-                  fontWeight={"500"}
-                  background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
-                  borderRadius="2px"
-                  paddingLeft={"4px"}
-                  paddingRight={"4px"}
-                >
-                  <BiDollar size="12px" color="black" />
-                  <Text
-                    as="span"
-                    marginLeft={"2px"}
-                    fontSize="11px"
-                    fontWeight={"bold"}
-                  >
-                    {video.price}
-                  </Text>
-                </Box>
-                <Box
-                  position={"absolute"}
-                  bottom="5px"
-                  color={"#000"}
-                  fontSize="11px"
-                  fontWeight={"bold"}
-                  right="5px"
-                  background={"none 0px 0px repeat scroll rgb(232, 232, 232)"}
-                  borderRadius="2px"
-                  padding={"0px 6px"}
-                >
-                  01:19
-                </Box>
-                <Box height="13em !important" width="100% !important">
-                  <Image
-                    height="13em !important"
-                    width="100% !important"
-                    borderRadius={"10px"}
-                    objectFit="cover"
-                    alt="test"
-                    src={`${video.thumbnail}`}
-                  />
-                </Box>
-              </Box>
-              <VideosTitle title={`${video.title}`} />
-              <Name username={`${video.username}`} />
-              <Text as="p" margin={"1px"}>
-                a day ago
-              </Text>
-            </GridItem>
-          ))}
-        </Grid>
+        {getTrendingFeed.loading ? (
+          <h1>&nbsp;</h1>
+        ) : (
+          <FeedGrid videos={getTrendingFeed.data.trendingFeed.items} />
+        )}
       </Box>
     </MainLayout>
   );
