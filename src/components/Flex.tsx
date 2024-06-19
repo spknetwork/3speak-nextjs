8a25265311df8ac4c35443cb9b23a8224e51c169
piyushjha0409 <peeyush0409@gmail.com>
2024-06-19 22:43:24 +0530

    Fix: Flex.tsx

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -4,7 +4,9 @@ import {
   flexbox,
   FlexboxProps,
   space,
+  Theme as StyledTheme,
   SpaceProps,
+  TLengthStyledSystem,
 } from "styled-system";
 
 import { Box } from "src/components";
@@ -12,7 +14,7 @@ import { Theme } from "src/styles/theme";
 
 const flexProps = compose(flexbox, space);
 
-interface Props<T> extends FlexboxProps<T>, SpaceProps<T> {}
+interface Props<T extends StyledTheme<TLengthStyledSystem>> extends FlexboxProps<T>, SpaceProps<T> {}
 
 export const Flex = styled(Box)<Props<Theme>>`
   display: flex;
