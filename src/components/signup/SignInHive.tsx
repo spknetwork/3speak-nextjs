79c07eaf53d4be75a9c74f0450207a23646b0af9
Geoffrey Casper <gcasper42@gmail.com>
2024-07-01 22:16:51 -0400

    hive auth

--- src/components/signup/SignInHive.tsx
+++ src/components/signup/SignInHive.tsx
@@ -1,36 +1,58 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-import {  Flex } from "src/components";
+import { Flex } from "src/components";
 import { Button, Box, Text } from "@chakra-ui/react";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import Image from "next/image";
+import { AuthActions } from "@/hooks/auth";
+import { Providers } from "@aioha/aioha";
 const SignInHive = ({ requestHiveLogin, username, setUsername }: any) => {
   return (
     <Box width="100%">
       <Box>
-        <Text as='h2' textAlign={'center'}>
+        <Text as="h2" textAlign={"center"}>
           Login to 3Speak
         </Text>
-        <Text textAlign={'center'}>
-          Select one of the supported login options that help keep your access safe and decentralized.
+        <Text textAlign={"center"}>
+          Select one of the supported login options that help keep your access
+          safe and decentralized.
         </Text>
       </Box>
       <Box mx="auto" maxWidth="9rem">
         <Image
-          loader={() => "https://s3.eu-central-1.wasabisys.com/data.int/logo_player.png"}
+          loader={() =>
+            "https://s3.eu-central-1.wasabisys.com/data.int/logo_player.png"
+          }
           src="https://s3.eu-central-1.wasabisys.com/data.int/logo_player.png"
           alt="3speak logo"
           width="100"
-          height={'100'}
+          height={"100"}
         />
       </Box>
       <form onSubmit={(e) => requestHiveLogin(e)}>
         <Flex>
-
-          <Flex width={'30rem'} borderRadius={'10px'} padding='10px' justifyContent={'center'} height={'50px'} backgroundColor={'black'} mt="11px" mr='10px'>
-            <Image width={'100'} height='100' src="/keychain.6846c271.png" alt="3speak logo" />
+          <Flex
+            width={"30rem"}
+            borderRadius={"10px"}
+            padding="10px"
+            justifyContent={"center"}
+            height={"50px"}
+            backgroundColor={"black"}
+            mt="11px"
+            mr="10px"
+          >
+            <Image
+              width={"100"}
+              height="100"
+              src="/keychain.6846c271.png"
+              alt="3speak logo"
+            />
           </Flex>
-          <Box marginRight={{ base: "10px", md: "10px", lg: "5px" }} mt="1rem" width="100%">
+          <Box
+            marginRight={{ base: "10px", md: "10px", lg: "5px" }}
+            mt="1rem"
+            width="100%"
+          >
             <fieldset className="Fieldset2">
               <input
                 value={username}
@@ -43,22 +65,53 @@ const SignInHive = ({ requestHiveLogin, username, setUsername }: any) => {
             </fieldset>
           </Box>
           <Box mt="1rem" width="auto">
-            <Button type="submit" height={'92%'} onClick={(e) => requestHiveLogin(e)}><FaLongArrowAltRight /></Button>
+            <Button
+              type="submit"
+              height={"92%"}
+              onClick={() =>
+                AuthActions.login("Hive", Providers.Keychain, username)
+              }
+            >
+              <FaLongArrowAltRight />
+            </Button>
           </Box>
-
         </Flex>
       </form>
-      <Flex justifyContent={"center"} backgroundColor={"grey.200"} opacity={0.3} position={"relative"} py={4}>
+      <Flex
+        justifyContent={"center"}
+        backgroundColor={"grey.200"}
+        opacity={0.3}
+        position={"relative"}
+        py={4}
+      >
         <Text position={"absolute"} left={72} fontSize={"2xl"} top={28}>
           Coming Soon
         </Text>
-        <Flex width={'30rem'} borderRadius={'10px'} padding='10px' justifyContent={'center'} height={'50px'} backgroundColor={'black'} mt="11px" mr='10px'>
-          <Image width={'100'} height='100' src="/hiveauth.ac85800f.svg" alt="3speak logo" />
+        <Flex
+          width={"30rem"}
+          borderRadius={"10px"}
+          padding="10px"
+          justifyContent={"center"}
+          height={"50px"}
+          backgroundColor={"black"}
+          mt="11px"
+          mr="10px"
+        >
+          <Image
+            width={"100"}
+            height="100"
+            src="/hiveauth.ac85800f.svg"
+            alt="3speak logo"
+          />
         </Flex>
-        <Box marginRight={{ base: "10px", md: "10px", lg: "5px" }} mt="1rem" width="100%">
+        <Box
+          marginRight={{ base: "10px", md: "10px", lg: "5px" }}
+          mt="1rem"
+          width="100%"
+        >
           <fieldset className="Fieldset2">
             <input
-              style={{ cursor: 'not-allowed' }}
+              style={{ cursor: "not-allowed" }}
               disabled={true}
               className="Input2"
               id="text"
@@ -67,28 +120,50 @@ const SignInHive = ({ requestHiveLogin, username, setUsername }: any) => {
             />
           </fieldset>
         </Box>
-        <Box cursor={'not-allowed'} mt="1rem" width="auto">
-          <Button height={'92%'} disabled={true}><FaLongArrowAltRight /></Button>
+        <Box cursor={"not-allowed"} mt="1rem" width="auto">
+          <Button height={"92%"} disabled={true}>
+            <FaLongArrowAltRight />
+          </Button>
         </Box>
       </Flex>
       <Flex backgroundColor={"grey.200"} opacity={0.3} position={"relative"}>
-        <Flex width={'30rem'} borderRadius={'10px'} padding='10px' justifyContent={'center'} height={'50px'} backgroundColor={'#d1d5da'} mt="11px" mr='10px'>
-          <Image width={'100'} height='100' src="/hivesigner.6958efa0.svg" alt="3speak logo" />
+        <Flex
+          width={"30rem"}
+          borderRadius={"10px"}
+          padding="10px"
+          justifyContent={"center"}
+          height={"50px"}
+          backgroundColor={"#d1d5da"}
+          mt="11px"
+          mr="10px"
+        >
+          <Image
+            width={"100"}
+            height="100"
+            src="/hivesigner.6958efa0.svg"
+            alt="3speak logo"
+          />
         </Flex>
-        <Box marginRight={{ base: "10px", md: "10px", lg: "5px" }} mt="1rem" width="100%">
+        <Box
+          marginRight={{ base: "10px", md: "10px", lg: "5px" }}
+          mt="1rem"
+          width="100%"
+        >
           <fieldset className="Fieldset2">
             <input
-              style={{ cursor: 'not-allowed' }}
+              style={{ cursor: "not-allowed" }}
               disabled={true}
               className="Input2"
               id="text"
               placeholder="Enter username"
-              type="text" 
+              type="text"
             />
           </fieldset>
         </Box>
         <Box mt="1rem" width="auto">
-          <Button height={'92%'} cursor={'not-allowed'} disabled={true}><FaLongArrowAltRight /></Button>
+          <Button height={"92%"} cursor={"not-allowed"} disabled={true}>
+            <FaLongArrowAltRight />
+          </Button>
         </Box>
       </Flex>
       {/* </form> */}
---
+++ src/hooks/auth/hive.ts
@@ -0,0 +1,87 @@
+import { Aioha, initAioha, Asset, KeyTypes, Providers } from "@aioha/aioha";
+import { Authenticator, AuthInfo } from "./types";
+import { AxiosRequestConfig } from "axios";
+
+const aioha =
+  typeof window === "undefined"
+    ? new Aioha()
+    : initAioha({
+        hiveauth: {
+          name: "3Speak",
+          // description: "Aioha test app",
+        },
+        hivesigner: {
+          app: "3speak.tv",
+          callbackURL: window.location.origin + "/hivesigner.html", // TODO set properly
+          scope: ["login", "vote"],
+        },
+      });
+
+function generatePayload(account: string) {
+  const payload = {
+    ts: Date.now(),
+    account,
+  };
+  const serializedPayload = JSON.stringify(payload);
+  return {
+    payload,
+    serializedPayload,
+  };
+}
+
+async function generateProofOfPayload(account: string) {
+  const { payload, serializedPayload: message } = generatePayload(account);
+
+  // const signature = privateKey.sign(
+  //   crypto.createHash("sha256").update(message).digest()
+  // );
+  // TODO double check the message is signed with the message hash
+  const signature = await aioha.signMessage(message, KeyTypes.Posting);
+  if (!signature.success) {
+    throw new Error(signature.error);
+  }
+  const proof = signature.result!;
+  console.log(`Signed Proof:\n${proof}, Proof: ${message}`);
+  return { proof, proof_payload: payload };
+}
+
+// // Step 2. Log in
+// const { data: dataLogin } = await axios.post(
+//   `https://staging.3speak.tv/api/v1/auth/login/singleton/hive`,
+//   {
+//     //   authority_type: "posting",
+//     proof_payload: pop[1],
+//     proof: pop[0],
+//   }
+// );
+
+export const hive = {
+  login(provider: Providers, username: string) {
+    const { payload, serializedPayload } = generatePayload(username);
+    return aioha
+      .login(provider, username, {
+        keyType: KeyTypes.Posting,
+        msg: serializedPayload,
+      })
+      .then((res) => {
+        if (res.success) {
+          const reqBody = {
+            proof_payload: payload,
+            proof: res.result,
+          };
+          return {
+            request: {
+              method: "POST",
+              data: reqBody,
+              url: "/v1/auth/login/singleton/hive",
+            },
+            userId: username,
+          } satisfies AuthInfo;
+        }
+        throw new Error(res.error);
+      });
+  },
+  logout() {
+    return aioha.logout();
+  },
+} satisfies Authenticator;
