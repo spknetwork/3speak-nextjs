810f03fd9f08b8b0cac52b2a58c131c6ffb518b5
piyush <peeyush0409@gmail.com>
2024-07-02 18:18:29 +0530

    Feat: Comments section UI upgrade complete

--- src/components/watch/Comment/AllComments.tsx
+++ src/components/watch/Comment/AllComments.tsx
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from "react";
-import {
-  Avatar,
-  Box,
-  Button,
-  Collapse,
-  Flex,
-  Link,
-  Text,
-} from "@chakra-ui/react";
-
+import { Box } from "@chakra-ui/react";
+import { InfinitySpin } from "react-loader-spinner";
 import { CommentInterface, VideoInterface } from "types";
 import { useAuth } from "@/hooks/auth";
 import { useGetComments } from "@/hooks/getComments";
 import SingleComment from "./SingleComment";
 type Props = {
   parentIndex?: number;
-  depth?: number
+  depth?: number;
   author: string;
   permlink: string;
   bgColor: string;
   colorMode: string;
 };
 
-const AllComments = ({ author, permlink, bgColor, colorMode, parentIndex, depth }: Props) => {
+const AllComments = ({ author, permlink, parentIndex, depth }: Props) => {
   const isAuthenticated = useAuth();
   const commentsData = useGetComments(author, permlink);
 
@@ -39,27 +30,30 @@ const AllComments = ({ author, permlink, bgColor, colorMode, parentIndex, depth 
     return allReplies;
   };
 
-
   if (commentsData === undefined) {
-    return <Box>Loading..</Box>;
+    return (
+    <Box>
+      <InfinitySpin width="100" color="#6DC5D7" />
+    </Box>
+    )
   }
   console.log(isAuthenticated);
 
   return (
     <Box>
       <Box overflow="hidden" position={"relative"} height={"auto"}>
-        <Box  paddingTop="12px" zIndex={2}>
-    {commentsData?.map((comment: CommentInterface) => (
-        <SingleComment
-          key={comment.permlink}
-          comment={comment}
-          parentIndex={parentIndex || 0}
-          depth={depth || 0}
-          defaultIsCollapsed={false}
-          author={author}
-          permlink={permlink}
-        />
-      ))}
+        <Box paddingTop="12px" zIndex={2}>
+          {commentsData?.map((comment: CommentInterface) => (
+            <SingleComment
+              key={comment.permlink}
+              comment={comment}
+              parentIndex={parentIndex || 0}
+              depth={depth || 0}
+              defaultIsCollapsed={false}
+              author={author}
+              permlink={permlink}
+            />
+          ))}
         </Box>
       </Box>
     </Box>
